859a9bccb5d067381a3d598e17eb15d8f14ebebd
OneFriendlyCoder <abhyanand311099@gmail.com>
2024-03-07 13:41:24 +0530

    authToken , refreshToken, deleting refreshToken

--- build/server.js
+++ build/server.js
@@ -18,13 +18,48 @@ const posts = [{
         title: 'post2'
     }
 ];
+let refreshTokens = [];
 app.get('/posts', authenticateToken, (req, res) => {
     res.json(posts.filter(post => post.username === req.user.name));
+});
+app.post('/token', (req, res) => {
+    const refreshToken = req.body.token;
+    if (refreshToken == null)
+        res.sendStatus(401);
+    if (!refreshTokens.includes(refreshToken))
+        res.sendStatus(403);
+    jsonwebtoken_1.default.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err)
+            res.sendStatus(403);
+        const accessToken = generateAccessToken({ name: user.name });
+        res.json({ accessToken: accessToken });
+    });
 });
 app.post('/login', (req, res) => {
     const username = req.body.username;
     const user = { name: username };
-    const accessToken = jsonwebtoken_1.default.sign(user, process.env.ACCESS_TOKEN_SECRET);
-    res.json({ accessToken: accessToken });
+    const accessToken = generateAccessToken(user);
+    const refreshToken = jsonwebtoken_1.default.sign(user, process.env.REFRESH_TOKEN_SECRET);
+    refreshTokens.push(refreshToken);
+    res.json({ accessToken: accessToken, refreshToken: refreshToken });
 });
+app.delete('/logout', (req, res) => {
+    refreshTokens = refreshTokens.filter(token => token !== req.body.token);
+    res.sendStatus(204);
+});
+function generateAccessToken(user) {
+    return jsonwebtoken_1.default.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30s' });
+}
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader === null || authHeader === void 0 ? void 0 : authHeader.split(' ')[1];
+    if (!token)
+        return res.sendStatus(401);
+    jsonwebtoken_1.default.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err)
+            return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+}
 app.listen(3000);
--- src/server.ts
+++ src/server.ts
@@ -20,8 +20,23 @@ const posts = [{
     title: 'post2'
 }
 ]
+
+let refreshTokens: string[] = [];
+
 app.get('/posts', authenticateToken ,(req: AuthenticatedRequest, res: express.Response) => { 
     res.json(posts.filter(post => post.username === req.user.name));
+})
+
+//refresh token is used to create a new access token
+app.post('/token', (req: AuthenticatedRequest, res: express.Response) => {
+    const refreshToken = req.body.token;
+    if(refreshToken == null) res.sendStatus(401);
+    if(!refreshTokens.includes(refreshToken)) res.sendStatus(403);
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET as Secret, (err: Error | null, user: any)=>{
+        if(err) res.sendStatus(403);
+        const accessToken = generateAccessToken({name: user.name})
+        res.json({accessToken: accessToken});
+})
 })
 
 app.post('/login', (req: express.Request, res: express.Response) => {
@@ -29,11 +44,24 @@ app.post('/login', (req: express.Request, res: express.Response) => {
 
     const username = req.body.username;
     const user = {name: username};
-    const accessToken  = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET as Secret);         // no expiration date of the accessToken
-    res.json({accessToken: accessToken});
+    const accessToken  = generateAccessToken(user);   // no expiration date of the accessToken
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET as Secret);    //this is used to create a new authorization token
+    refreshTokens.push(refreshToken);
+    res.json({accessToken: accessToken, refreshToken: refreshToken});
 })
 
-/*
+
+app.delete('/logout', (req: express.Request, res: express.Response) => {
+    refreshTokens = refreshTokens.filter(token =>token !== req.body.token)
+    res.sendStatus(204);
+})
+
+function generateAccessToken(user: {name: string}){
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET as Secret, {expiresIn: '30s'})
+}
+
+
+
 function authenticateToken(req: AuthenticatedRequest, res: express.Response, next: express.NextFunction) {
     const authHeader = req.headers['authorization'];
     const token = authHeader?.split(' ')[1];
@@ -45,5 +73,5 @@ function authenticateToken(req: AuthenticatedRequest, res: express.Response, nex
         next();
     })
 }
-*/
+
 app.listen(3000);
